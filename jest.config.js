3302f2a10deadf62adffed16ffc280ea98f5b223
lina-nguyen <CLNT417D@spgroup.com.sg>
2023-10-05 10:58:30 +0700

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/__test__/**',
+    '!**/index.ts',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/vendor/**'
+  ]
 };
